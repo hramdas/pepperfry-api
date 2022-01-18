28b081f0a5fb51eba612f4aabd451c2fb908387b
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-18 23:10:15 +0530

    wishlist

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const connect = require('./configs/db')
 const app = require('./index')
 const port =  process.env.PORT || 3000
 
-app.listen(port, async ()=>{
+app.listen(5000, async ()=>{
     await connect()
     console.log("Listening on port 3000")
 })
