0325412aab70d7afa84b705b501f20a858cdc669
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-19 10:39:37 +0530

    change api

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 module.exports = ()=>{
-    // return mongoose.connect('mongodb://127.0.0.1:27017/pepperfry')
     return mongoose.connect(process.env.DB_URL)
 }
