e7d02521aaa5436418be44f6c868ecb98071d14f
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-03-16 15:13:57 +0530

    final

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-
-const connect = require('./src/configs/db')
-const app = require('./src/index')
-const port =  process.env.PORT || 3001
-
-app.listen(port, async ()=>{
-    await connect()
-    console.log("Listening on port 3001")
-})
-
+const connect = require("./src/configs/db");
+const app = require("./src/index");
+const port = process.env.PORT || 3001;
+app.listen(port, async () => {
+  await connect();
+  console.log("Listening on port 3001");
+});
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-var cors = require('cors')
-app.use(cors())
+var cors = require("cors");
+app.use(cors());
 
-const userController = require('./controllers/users')
-app.use('/user', userController)
+const userController = require("./controllers/users");
+app.use("/user", userController);
 
-const productController = require('./controllers/products')
-app.use('/product', productController)
+const productController = require("./controllers/products");
+app.use("/product", productController);
 
-app.get('/', async(req, res)=>{
-    res.status(200).json("welcome")
-})
+app.get("/", async (req, res) => {
+  res.status(200).json("welcome");
+});
 
-const cartController = require('./controllers/carts')
-app.use('/cart', cartController)
+const cartController = require("./controllers/carts");
+app.use("/cart", cartController);
 
-const widhlistController = require('./controllers/wishlists')
-app.use('/wishlist', widhlistController)
+const widhlistController = require("./controllers/wishlists");
+app.use("/wishlist", widhlistController);
 
-module.exports = app
+module.exports = app;
